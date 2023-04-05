79d512d9282ae338bf18e6fb14aff98e840192a3
harshj-1703 <harshj6680@gmail.com>
2023-04-05 19:02:49 +0530

    faster made

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import pandas as pd
 from numpy.linalg import norm
 from pymongo import MongoClient
 import json
+from sentence_transformers import util
+import torch
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['ai_elective_subject']
@@ -30,6 +32,7 @@ chain8 = data['chain8']
 chain9 = data['chain9']
 chain10 = data['chain10']
 cosine_similarities = []
+trained_des = data['trained_des']
 
 # function
 def checkIndexOrNot(output1,chain_zip):
@@ -39,7 +42,7 @@ def checkIndexOrNot(output1,chain_zip):
 @app.route('/',methods=['POST','GET'])
 def results():
     # form = request.form
-    embeddings = []
+    # embeddings = []
     cosine_similarities = []
     if request.args.get('semester') is not None and request.args.get('description') != '':
         # c_des.append(request.args.get('description'))
@@ -59,20 +62,24 @@ def results():
 
         semester = request.args.get('semester')
         description = request.args.get('description')
-        c_des.append(description)
-        embeddings = model.encode(c_des)
-        embeded = np.array(embeddings)
-        array_len = len(embeded)
+        # c_des.append(description)
+        # embeddings = model.encode(c_des)
+        # embeded = np.array(embeddings)
+        # array_len = len(embeded)
         # Compute dot product of the last embedding with all other embeddings
-        dot_product = np.dot(embeded[array_len-1], embeded.T)
-        # Compute norms of all embeddings
-        norms = np.linalg.norm(embeded, axis=1)
-        # Compute cosine similarities
-        cosine_similarities = dot_product / (norms[array_len-1] * norms)
-        # Remove the last cosine similarity as it corresponds to the dot product of the last embedding with itself
-        cosine_similarities = cosine_similarities[:-1]
-        c_des.pop()
-        index = np.argmax(cosine_similarities)
+        # dot_product = np.dot(embeded[array_len-1], embeded.T)
+        # # Compute norms of all embeddings
+        # norms = np.linalg.norm(embeded, axis=1)
+        # # Compute cosine similarities
+        # cosine_similarities = dot_product / (norms[array_len-1] * norms)
+        # # Remove the last cosine similarity as it corresponds to the dot product of the last embedding with itself
+        # cosine_similarities = cosine_similarities[:-1]
+        # c_des.pop()
+
+        description = model.encode(description)
+        cosine_similarities = util.dot_score(description, trained_des)
+        index = (np.argmax(cosine_similarities)).item()
+        print(index)
         output1 = courses_dataset['Course Name'][index]
         output1_sem = courses_dataset['Semester'][index]
 
@@ -98,7 +105,7 @@ def results():
         else:
             final_chain = chain10
 
-        user_sem = int(request.args.get('semester'))
+        user_sem = int(semester)
         final = [{k:v} for k,v in final_chain.items() if v >= user_sem]
         output_dict = {k: v for d in final for k, v in d.items()}
         if(output1 in output_dict):
--- test.py
+++ test.py
@@ -1,15 +1,59 @@
+# # inport library
+# from flask import Flask, render_template, request
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sentence_transformers import SentenceTransformer
+# from numpy.linalg import norm
+# import pickle
+
+# courses_dataset = pd.read_excel('./model train files/Elective Courses.xlsx')
+# c_des = list(courses_dataset['Description'])
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# # similar subjects
+# chain1={"Advanced Microprocessor" : 5,"Embedded System Design" : 6,"VLSI Designs" : 6,"Digital Design using Verilog" : 7,"VLSI Physical Design" :8,"FPGA Based System Design" :8,"Embedded Linux" :8,"Internet of Things":7}
+# chain2={"Optical Communication" : 5,"RF and Microwave Communication" : 6,"Satellite Communication" : 6,"Wireless system Design" : 7,"Multimedia computing" : 7,"Spread Spectrum communications" : 8}
+# chain3={"Linux Administration" : 5,"Cloud Computing" : 6,"Embedded Operating System" : 7,"Introduction to DevOps Tools" : 8,"Network Administration" : 8,"Advanced Computer Networks" : 6}
+# chain4={"Applied Linear algebra" : 5,"Machine learning" : 6,"Data Warehousing and Data mining" : 6,"Big Data Analytics" : 7,"Computer Vision" : 7,"Human computer interaction" : 7,"Data Visualization" : 8,"Deep Learning for Computer Vision" : 8}
+# chain5={"Theory of Computation" : 5,"Compiler Design" : 6}
+# chain6={".NET Technology" : 6,"Advance Web Technology" : 8}
+# chain7={"Programming for Application Development" : 7,"Cross Platform Mobile Development" : 8}
+# chain8={"Game Development" : 8}
+# chain9={"Advance Java" : 7,"Advance Database" : 8,"Advance C++ Programming" : 8}
+# chain10={"SEO And Digital Marketing" : 8}
+
+# data = {'courses_dataset': courses_dataset,
+#         'c_des': c_des,
+#         'model': model,
+#         'chain1': chain1,
+#         'chain2': chain2,
+#         'chain3': chain3,
+#         'chain4': chain4,
+#         'chain5': chain5,
+#         'chain6': chain6,
+#         'chain7': chain7,
+#         'chain8': chain8,
+#         'chain9': chain9,
+#         'chain10': chain10}
+
+# with open('model.pickle', 'wb') as f:
+#   pickle.dump(data, f)
+
 # inport library
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer,util
 from numpy.linalg import norm
 import pickle
 
 courses_dataset = pd.read_excel('./model train files/Elective Courses.xlsx')
 c_des = list(courses_dataset['Description'])
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+
+trained_des = model.encode(c_des)
 
 # similar subjects
 chain1={"Advanced Microprocessor" : 5,"Embedded System Design" : 6,"VLSI Designs" : 6,"Digital Design using Verilog" : 7,"VLSI Physical Design" :8,"FPGA Based System Design" :8,"Embedded Linux" :8,"Internet of Things":7}
@@ -25,6 +69,7 @@ chain10={"SEO And Digital Marketing" : 8}
 
 data = {'courses_dataset': courses_dataset,
         'c_des': c_des,
+        'trained_des' : trained_des,
         'model': model,
         'chain1': chain1,
         'chain2': chain2,
